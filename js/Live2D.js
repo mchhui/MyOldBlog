2c9c4db9395e0784494eb8eacdaf55221c3b12f3
mchhui <mchhui@foxmail.com>
2019-04-29 20:32:23 +0800

    Site updated: 2019-04-29 20:32:22

--- js/Live2D.js
+++ js/Live2D.js
@@ -17,8 +17,9 @@ var oldonload = window.onload
 window.onload=(function() {
 	$('#live2dcanvas')[0].style.opacity="0.5";
 	$('#live2dcanvas')[0].style.marginTop="130px";
-	$("body").append('<div id="l2dTipBar" style="position: fixed; right: 50px; bottom: 160px; width: 200px;  z-index: 100000; opacity: 1; background: rgb(245,245,245); border: 2px solid #777; border-radius: 20px;opacity:0;text-align:center;padding:10px 5px 10px 5px;pointer-events:none;"><font id="l2dTip" color="#222" size="2"></font></div>');
-    $("body").append("<div id='chatBar' style='position: fixed; right: 250px;bottom: 20px;opacity: 0;'><div style='width:150px;height:19px;background: rgb(245,245,245); border: 2px solid #777;'><input id='turing_input_box' type='text' placeholder='我想说...' style='vertical-align: top;font-family:SimSun;font-size:10px;width:130px;height:17px;border:0px;background:none;margin-left:10px;outline:none;'/></div><button id='send_button' onclick='turing_submit();clearChat();' background: style='width:75px;height:19px;background:rgb(60,179,113); border: 1px solid #777; border-radius: 5px;font-size:10px;'>发射</button><button id='clear_button' onclick='clearChat()' background: style='width:75px;height:19px;background:rgb(0,191,255); border: 1px solid #777; border-radius: 5px;margin-left:4px;font-size:10px;'>清空</button></div>")
+    $('#live2dcanvas')[0].style.marginLeft="50px";
+	$("body").append('<div id="l2dTipBar" style="position: fixed; right: 10px; bottom: 160px; width: 200px;  z-index: 100000; opacity: 1; background: rgb(245,245,245); border: 2px solid #777; border-radius: 20px;opacity:0;text-align:center;padding:10px 5px 10px 5px;pointer-events:none;"><font id="l2dTip" color="#222" size="2"></font></div>');
+    $("body").append("<div id='chatBar' style='position: fixed; right: 170px;bottom: 20px;opacity: 0;'><div style='width:150px;height:19px;background: rgb(245,245,245); border: 2px solid #777;'><input id='turing_input_box' type='text' placeholder='我想说...' style='vertical-align: top;font-family:SimSun;font-size:10px;width:130px;height:17px;border:0px;background:none;margin-left:10px;outline:none;'/></div><button id='send_button' onclick='turing_submit();clearChat();' background: style='width:75px;height:19px;background:rgb(60,179,113); border: 1px solid #777; border-radius: 5px;font-size:10px;'>发射</button><button id='clear_button' onclick='clearChat()' background: style='width:75px;height:19px;background:rgb(0,191,255); border: 1px solid #777; border-radius: 5px;margin-left:4px;font-size:10px;'>清空</button></div>")
    setInterval(checkTip,20);
     setTimeout(function(){msgTip("鸭鸭,又见面了！<br>双击空白处也可以唤出我哦<br>更新计划:<br>1.回复栏");},200);
     touch.on($("body"),"doubletap",function(e){                
@@ -78,10 +79,10 @@ document.onclick=function(ev){                
 	var y=oEvent.clientY;
 	//popup(document.body.clientHeight-y,-1);
 
-		if(isShow&&document.body.clientWidth-x>100&&document.body.clientWidth-x<200&&document.body.clientHeight-y<160){
+		if(isShow&&document.body.clientWidth-x>50&&document.body.clientWidth-x<150&&document.body.clientHeight-y<160){
 			msgTip("好吧好吧qwq 不烦你了XD");
 			hide();
-		}else if(document.body.clientWidth-x>100&&document.body.clientWidth-x<200&&document.body.clientHeight-y<90){
+		}else if(document.body.clientWidth-x>50&&document.body.clientWidth-x<150&&document.body.clientHeight-y<90){
 			msgTip("我来咯！");
 			show();
 			isInLive2d=true;
@@ -96,7 +97,7 @@ $(document).mousemove(function(ev){
 	var x=e.clientX;
 	var y=e.clientY;
 		if(isShow){
-			if(document.body.clientWidth-x>100&&document.body.clientWidth-x<200&&document.body.clientHeight-y<170){
+			if(document.body.clientWidth-x>50&&document.body.clientWidth-x<150&&document.body.clientHeight-y<170){
 				if(isInLive2d){
 				return;	
 				}
@@ -135,7 +136,7 @@ function talk(str){
 }
 var xhr = new XMLHttpRequest;
 function turing_submit() {
-  var url = "https://www.tuling123.com/openapi/api?key=e1230e4385c8a5efdb7ae21a293f9f0c&info=" + document.getElementById("turing_input_box").value;
+  var url = "//www.tuling123.com/openapi/api?key=e1230e4385c8a5efdb7ae21a293f9f0c&info=" + document.getElementById("turing_input_box").value;
   xhr.open("get",url,false);
   xhr.send(null);
   if ( (xhr.status >= 200 && xhr.status < 300) || xhr.status == 304 ){
