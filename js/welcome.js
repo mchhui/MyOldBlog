7bdeaa18b96cabc6ec0f03f523454ebad8e73b2f
mchhui <mchhui@foxmail.com>
2019-05-03 20:23:50 +0800

    Site updated: 2019-05-03 20:23:49

--- js/welcome.js
+++ js/welcome.js
@@ -1,9 +1,14 @@
 onloadManager.addFunction(function(){
-    if(document.cookie=="isWelcomed=true"){
-        return;
+    document.cookie="welcomeTime="+new Date().getTime();
+    if(document.cookie==""){
+        welcome();
+        document.cookie="welcomeTime="+new Date().getTime();
+        return
     }
+    if(new Date().getTime()-parseInt(document.cookie.split("=")[1])>300000){
     welcome();
-    document.cookie="isWelcomed=true";
+    }
+    document.cookie="welcomeTime="+new Date().getTime();
 })
 function welcome(){
     $("body").append("<div id='welcomeBar' style='position: fixed; z-index:999;left: 50%;bottom: 60%;opacity: 1;'><div style='text-align:center;margin-left:-43%;'><img id='welcomeimg' src='/mchhui/assets/img/welcomeimg.png' alt='图片加载失败'' style='width:80%;height:auto;margin-left:-40%;'></div></div>")
