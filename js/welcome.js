837af06a77de54325c97f5ffb7b718c968f364f6
mchhui <mchhui@foxmail.com>
2019-05-03 20:36:48 +0800

    Site updated: 2019-05-03 20:36:47

--- js/welcome.js
+++ js/welcome.js
@@ -11,7 +11,7 @@ onloadManager.addFunction(function(){
     document.cookie="welcomeTime="+new Date().getTime();
 })
 function welcome(){
-    $("body").append("<div id='welcomeBar' style='position: fixed; z-index:999;left: 50%;bottom: 60%;opacity: 1;'><div style='text-align:center;margin-left:-43%;'><img id='welcomeimg' src='/mchhui/assets/img/welcomeimg.png' alt='图片加载失败'' style='width:80%;height:auto;margin-left:-40%;' onclick='javascript:$('#welcomeBar').remove();'"></div></div>")
+    $("body").append("<div id='welcomeBar' style='position: fixed; z-index:999;left: 50%;bottom: 60%;opacity: 1;'><div style='text-align:center;margin-left:-43%;'><img id='welcomeimg' src='/mchhui/assets/img/welcomeimg.png' alt='图片加载失败'' style='width:80%;height:auto;margin-left:-40%;' onclick='javascript:$('#welcomeBar').remove();''></div></div>")
 setTimeout(function(){
     interval =setInterval(function(){
     $('#welcomeimg')[0].style.width=parseInt(parseInt($('#welcomeimg')[0].style.width)-4)+'%';
