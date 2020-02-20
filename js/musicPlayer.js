9fb6157c06dc59106c1e440b773061d154cdddf6
mchhui <mchhui@foxmail.com>
2020-02-20 23:37:29 +0800

    Site updated: 2020-02-20 23:37:26

--- js/musicPlayer.js
+++ js/musicPlayer.js
@@ -3,6 +3,6 @@ if(location.pathname=="/"){
     return;
 }
 	$('.article-entry').append("<div style='text-align:right'>"+
-    "<embed src='//music.163.com/style/swf/widget.swf?sid=2774773432&type=0&auto=1&width=310&height=430' width='350' height='470'  allowNetworking='all'></embed>"+
+    "<iframe frameborder=\"no\" border=\"0\" marginwidth=\"0\" marginheight=\"0\" width=330 height=450 src=\"\/\/music.163.com/outchain/player?type=0&id=2774773432&auto=1&height=430\"></iframe>"+
     "</div>");
 })
